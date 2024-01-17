bb629594a727d2f577dc0b1ab4503b7e1c93758b
David Grudl <david@grudl.com>
2024-01-17 14:32:01 +0100

    minimal version

--- app/RouterFactory.php
+++ app/RouterFactory.php
@@ -2,20 +2,17 @@
 
 declare(strict_types=1);
 
-namespace App\Router;
+namespace App;
 
-use Nette;
 use Nette\Application\Routers\RouteList;
 
 
 final class RouterFactory
 {
-	use Nette\StaticClass;
-
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
+		$router->addRoute('<presenter>/<action>', 'Home:default');
 		return $router;
 	}
 }
